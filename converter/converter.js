69c850532743664fdd12267cd1f51af7a1f5e574
k7fp <7762536+k7fp@users.noreply.github.com>
2024-04-09 13:43:57 -0700

    Missing body content and request content type handling

--- converter/converter.js
+++ converter/converter.js
@@ -24,18 +24,50 @@ class RestClientConverter {
                     break;
                 case 'POST':
                     console.log('POST ' + ep.path);
-                    console.log('Content-Type: ' + Object.keys(ep.requestBody.content)[0]);
-                    console.log(ep.requestBody.content);
-                    break;
+                    // console.log(ep.requestBody.content[Object.keys(ep.requestBody.content)[0]]);
+                    // let content = ep.requestBody.content[Object.keys(ep.requestBody.content)[0]] ?? null;
 
+                    try {
+                        // console.log(ep.requestBody.content);
+                        for (const [enctype, content] of Object.entries(ep.requestBody.content)) {
+                            console.log('Content-Type: ' + enctype);
+
+                            let schema = content['schema'] ?? null;
+                            let properties = schema['properties'] ?? null;
+                            // console.log(properties);
+
+                            // TODO: if not one of the recognized content-types, do we skip. Can do different handling for multipart/form-data and application/json for example
+                            switch (enctype) {
+                                case 'multipart/form-data':
+                                    // for (const [key, property] of Object.entries(properties)) {
+                                    //     console.log(key);
+                                    // }
+                                    break;
+                                case 'application/json':
+                                    break;
+                                default:
+                            }
+                        }
+                    } catch (err) {
+                        console.log('Unable to parse content for POST' + ep.path);
+                    }
+                    break;
                 case 'PUT':
-                    console.log('PUT ' + ep.path);
-                    console.log('Content-Type: ' + Object.keys(ep.requestBody.content)[0]);
+                    try {
+                        console.log('PUT ' + ep.path);
+                        console.log('Content-Type: ' + Object.keys(ep.requestBody.content)[0]);
+                    } catch (err) {
+                        console.log('Unable to parse content for PUT' + ep.path);
+                    }
                     break;
 
                 case 'PATCH':
-                    console.log('PATCH ' + ep.path);
-                    console.log('Content-Type: ' + Object.keys(ep.requestBody.content)[0]);
+                    try {
+                        console.log('PATCH ' + ep.path);
+                        console.log('Content-Type: ' + Object.keys(ep.requestBody.content)[0]);
+                    } catch (err) {
+                        console.log('Unable to parse content for PATCH' + ep.path);
+                    }
                     break;
 
                 case 'DELETE':
