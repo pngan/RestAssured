b7a7b6b3b8f317efc060ce18aa3f0977f5800965
Alan Proctor <alan.proctor@xero.com>
2024-04-11 14:47:09 -0600

    add file upload to conversion

--- src/Components/SourceInputSelection.js
+++ src/Components/SourceInputSelection.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import { useApp } from "../AppProvider";
 import styled from 'styled-components';
 import { InputWrapper, Label } from '../styles/sharedStyles'
@@ -7,7 +7,13 @@ const LoadButton = styled.button`
   margin-left: 10px;
   flex-grow: 1;
 `
-
+const BrowseButton = styled.button`
+  margin-left: 10px;
+  flex-grow: 1;
+`
+const FileInput = styled.input`
+  display: none;
+`
 const SourceInput = styled.input`
   flex-grow:10;
 `
@@ -21,7 +27,7 @@ const InputButtonWrapper = styled.div`
 const SourceInputSelection = (props) => {
   const [fileOrUrl, setFileOrUrl] = useState('https://petstore3.swagger.io/api/v3/openapi.json');
   const { loadOpenApiContentFromFileOrRequest } = useApp();
-  
+  const inputElement = useRef();
   const updateSourceValue = (event) => {
     // add debounce
     setFileOrUrl(event.target.value)
@@ -37,11 +43,21 @@ const SourceInputSelection = (props) => {
     } 
   };
 
+  const fileUploadSelect = () => {
+    inputElement.current.click();
+  };
+
+  const fileInputSelection = () => {
+    setFileOrUrl(inputElement.current?.files[0]?.path);
+  }
+
   return (
     <InputWrapper>
       <Label>URL or File:</Label>
       <InputButtonWrapper>
         <SourceInput type='text' onChange={updateSourceValue} value={fileOrUrl} />
+        <FileInput onChange={fileInputSelection} type={'file'} ref={inputElement} />
+        <BrowseButton onClick={fileUploadSelect}>Browse</BrowseButton>
         <LoadButton type="button" onClick={loadOpenApiContent}>Load</LoadButton>
       </InputButtonWrapper>
     </InputWrapper>
